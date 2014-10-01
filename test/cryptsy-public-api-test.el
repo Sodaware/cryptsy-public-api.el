0ad222b70ceaf6d5185744c5a53233a050ebfd45
Phil Newton <phil@sodaware.net>
2014-09-30 21:18:10 -0400

    Added test for cryptsy-public-api-get-info-value helper

--- test/cryptsy-public-api-test.el
+++ test/cryptsy-public-api-test.el
@@ -9,3 +9,9 @@
            "method=singlemarketdata&marketid=182"
            (cryptsy-public-api-build-query (list :method "singlemarketdata"
                                                  :marketid 182)))))
+
+(ert-deftest cryptsy-public-api-test/can-get-named-value ()
+  (let ((contents (read-fixture "singlemarketdata-marketid-182.json")))
+    (should (string= "DOGE"
+                     (cryptsy-public-api-get-info-value 'DOGE 'primarycode contents)))))
+
